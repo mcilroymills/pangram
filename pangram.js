a3d2e7312c8b8e615b576319e96decd57a5c6ddd
millsmcilroy <millsmcilroy@gmail.com>
2016-04-14 10:07:44 -0600

    refactored down to 13 lines

--- pangram.js
+++ pangram.js
@@ -1,27 +1,16 @@
-
 module.exports = function (sentence) {
-
   //Alphabet array to compare with sentence
   this.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
   //Returns an array of letters that alphabet & sentence have in common
   this.alphaArray = function () {
     return this.alphabet.filter(function (char) {
-      if (sentence.toLowerCase().indexOf(char) === -1)
-        return false;
-      else
-        return true;
+      return sentence.toLowerCase().includes(char);
     });
   };
 
   //Returns true if sentence is a pangram, false otherwise
   this.isPangram = function () {
-
-    var array = this.alphaArray();
-
-    if (array.length === this.alphabet.length)
-      return true;
-    else
-      return false;
+    return this.alphaArray().length === this.alphabet.length;
   };
-}
+};
